aad623dc4175ff8be891385349376c723a5cbcea
xugangfm <xugang@xugangs-MacBook-Pro.local>
2014-08-11 03:14:09 +0800

    first

---
+++ TestPthread/TestPthread/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestPthread
+//
+//  Created by xugang on 8/11/14.
+//  Copyright (c) 2014 RongCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestPthread/TestPthread/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestPthread
+//
+//  Created by xugang on 8/11/14.
+//  Copyright (c) 2014 RongCloud. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
